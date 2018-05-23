f0aa1c58087c145813a59ec4552b7b41862a1eeb
Jaroslav Surala <SuralaJ@gmail.com>
2018-05-23 18:48:30 +0200

    Initial project structure

---
+++ src/Blazor.MaterialUI/ExampleJsInterop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace Blazor.MaterialUI
+{
+    public class ExampleJsInterop
+    {
+        public static string Prompt(string message)
+        {
+            return RegisteredFunction.Invoke<string>(
+                "Blazor.MaterialUI.ExampleJsInterop.Prompt",
+                message);
+        }
+    }
+}
