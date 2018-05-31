7cf0d6c2791a9b8a580907d591a996d2ac81adfa
Jaroslav Surala <SuralaJ@gmail.com>
2018-05-31 19:17:44 +0200

    Update docs + Add Blazor standalone demo

---
+++ demo/DemoAppStandalone/Program.cs
@@ -0,0 +1,20 @@
+﻿using Blazor.Extensions.CssStyles;
+using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+
+namespace DemoAppStandalone
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(services =>
+            {
+                // Add any custom services here
+                services.AddCss();
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+    }
+}
